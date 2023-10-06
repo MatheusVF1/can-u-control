114001b99f0d71e1b2fe3bf8a4e4c72a7f64d58b
Matheus Vieira <90991829+MatheusVF1@users.noreply.github.com>
2023-10-06 13:43:12 -0300

    Update euler.py

--- euler.py
+++ euler.py
@@ -22,18 +22,53 @@ def maximum(land, level):
             uMax = 10
     return uMax
 
+
+
 def calculate_distances(x, y, land, level, quant, dt, BonusX, BonusY, SkullsX, SkullsY):
+    """
+    Calcula a melhor direção que o jogador deve tomar para minimizar a distância até todos os bônus.
+
+    Args:
+        x: A coordenada x do jogador.
+        y: A coordenada y do jogador.
+        land: O objeto do terreno.
+        level: O objeto do nível.
+        quant: O fator de quantização.
+        dt: O passo de tempo.
+        BonusX: Uma lista de coordenadas x dos bônus.
+        BonusY: Uma lista de coordenadas y dos bônus.
+        SkullsX: Uma lista de coordenadas x dos crânios.
+        SkullsY: Uma lista de coordenadas y dos crânios.
+
+    Returns:
+        O melhor valor de `u`.
+    """  
+    
+    # Inicializa a melhor distância como infinito
     bestDist = np.inf
-    bestI = 0        
+
+    # Inicializa o melhor valor de `u` como zero.
+    bestI = 0 
+
+    # Calcula o máximo valor de `u` para o terreno e o nível dados.
     uMax = maximum(land, level)
+
+    # Para cada valor de `u` de `-uMax` a `uMax`, calcula a distância até todos os bônus simulando o movimento do jogador por `quant` passos de tempo.
+
     for i in range(-uMax, uMax + 1):
+
+         # Calcula o valor de `u` atual dividido por `uMax`.
         valueUI = i / uMax
+
+        # Inicializa a distância mínima até todos os bônus para o valor atual de `u` como infinito.
         minimumDistance = np.inf
         distance = np.inf
         x1 = x
         y1 = y
         u = valueUI
         for k in range(quant):
+            
+            # Para cada passo de tempo, a função calcula a distância até o bônus mais próximo. Se a distância for menor que minimumDistance, minimumDistance é atualizada.
             for q in range(len(BonusX)):
                 f = 1 if q == 0 else 2
                 distance = f*((x1 - BonusX[q])*(x1 - BonusX[q]) + (y1 - BonusY[q])*(y1 - BonusY[q]))
@@ -42,6 +77,7 @@ def calculate_distances(x, y, land, level, quant, dt, BonusX, BonusY, SkullsX, S
             l12 = define_maps_equations(land, level, x1, y1, u)
             x1 += l12[0]*dt
             y1 += l12[1]*dt
+            # Se minimumDistance for menor que bestDist, bestDist é atualizada com o novo valor.
             if(minimumDistance < bestDist):
                 bestDist = minimumDistance
                 bestI = valueUI
